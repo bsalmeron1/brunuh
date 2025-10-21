75a5eadd3bb5ad6a51ae293db1cf5576e422df22
Tapia <bsalmeron1@cps.k12.il.us>
2025-10-21 15:09:51 -0500

    first commit

---
+++ lesson1.py
@@ -0,0 +1,28 @@
+#g
+print("Hello, world")
+name= "jack"
+#integers are wole nums without decimal points
+#STRINGS are enclosed with quotes
+age = 4
+#FLOATS are numbers wtih decimal points
+is_student = False 
+#true or false statemeants are booleans
+
+print ("name:", name )
+print ("age:", age)
+print ("is_student:", is_student
+)
+#concadination- represented by + conjointing strings
+
+print("Hello, my name is " +name+ ",I am " +str(age)+ "years old, this is all " +str(is_student) ) 
+
+#F STRING
+print (f"Hello, my name is {name}, I am {age} years old")
+
+city = "Chicago"
+num_of_pals =67
+favorite_drink = "chai"
+favorite_season= "fall"
+dog= "Hemphry"
+
+print(f" Hi there, I live in {city}, I have made {num_of_pals} friends! We al like to drink {favorite_drink} during {favorite_season}. We also like to play with {dog} , my pet cat.")
